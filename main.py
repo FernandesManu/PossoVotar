e6e2e1429fdcd9f522180bf488716f35a89a4581
Manuella-Fernandes <178603951+FernandesManu@users.noreply.github.com>
2026-05-22 11:41:56 +0000

    add if

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ ano_atual = 2026
 calculo = ano_atual - usuario_nasc
 
 print(f'hoje você tem {calculo} anos')
+
+idade_votar = 16
+
+if calculo >= idade_votar:
+    print('pode votar')
+else:
+    print('ainda não pode votar')
